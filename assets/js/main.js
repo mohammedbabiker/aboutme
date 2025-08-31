618a8aa37b6592d728b8b952cd9cfacca109097a
Mohammed Babiker <mobabikerbabai20@gmail.com>
2025-08-31 18:38:53 +0300

    formate files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+const $navMenu = $("#nav-menu"),
+  $navToggle = $("#nav-toggle"),
+  $navClose = $("#nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+if ($navToggle) {
+  $navToggle.on("click", () => {
+    $navMenu.addClass("show-menu");
   });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+if ($navClose) {
+  $navClose.on("click", function () {
+    $navMenu.removeClass("show-menu");
   });
 }
 
@@ -23,9 +23,9 @@ if (navClose) {
 const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-  const navMenu = document.getElementById("nav-menu");
+  const $navMenu = $("#nav-menu");
   // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove("show-menu");
+  $navMenu.removeClass("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
